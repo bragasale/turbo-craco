b53b8e3b488092cb03dfb00e578acc8217138b8f
Ricardo Braganca <bragasale@hotmail.com>
2019-09-04 16:04:47 -0400

    Json Return Formmat

--- cracoserver/server.js
+++ cracoserver/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.get('/',function(req,res){
+    res.json({title:'hello world'})
+})
 app.use(express.static('public'))
 
 app.listen(process.env.PORT || 8080, () => console.log('All is ok'))
